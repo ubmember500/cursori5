d25423b2c88db89fd4af8dcf2f24d077f10fc84b
ubmember500 <ubmember500@gmail.com>
2025-03-12 17:15:18 +0200

    Add user_loader and 500 error template

--- app.py
+++ app.py
@@ -154,6 +154,10 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 # Middleware для защиты от XSS
 def xss_protection_middleware():
     def sanitize_input(text):
