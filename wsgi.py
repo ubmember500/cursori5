84e89f91537c1dcfc4ac9cbb87e479f83ce38735
ubmember500 <ubmember500@gmail.com>
2025-03-13 13:38:14 +0200

    Добавлены настройки email в wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,15 @@ path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if path not in sys.path:
     sys.path.append(path)
 
+# Настройки email
+os.environ['MAIL_SERVER'] = 'smtp.gmail.com'
+os.environ['MAIL_PORT'] = '587'
+os.environ['MAIL_USE_TLS'] = 'True'
+os.environ['MAIL_USERNAME'] = 'defensivelox@gmail.com'
+os.environ['MAIL_PASSWORD'] = 'pgqm xxgl srss caue'
+os.environ['MAIL_DEFAULT_SENDER'] = 'defensivelox@gmail.com'
+os.environ['ADMIN_EMAIL'] = 'defensivelox@gmail.com'
+
 # Импортируем Flask-приложение
 from app import app as application
 
