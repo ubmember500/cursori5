d8eeddb27afeb4e9116a3ce1feef7123b9ab0d7b
Sala <defensivelox@gmail.com>
2025-03-14 17:48:18 +0200

    Пересоздана база данных с правильной структурой

--- migrations/versions/acaccbc16f03_initial_migration.py
+++ migrations/versions/acaccbc16f03_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial migration
 
-Revision ID: 09aa683f0d87
+Revision ID: acaccbc16f03
 Revises: 
-Create Date: 2025-03-14 17:43:44.763755
+Create Date: 2025-03-14 17:47:51.369086
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '09aa683f0d87'
+revision = 'acaccbc16f03'
 down_revision = None
 branch_labels = None
 depends_on = None
