91137997cece9dc32a42fcbb1d2e23d8015e6825
Sala <defensivelox@gmail.com>
2025-03-16 21:16:14 +0200

    Исправлен маршрут в функции login: изменен redirect на home вместо index

--- app.py
+++ app.py
@@ -936,7 +936,7 @@ def login():
             # Перенаправляем на запрошенную страницу или на главную
             next_page = request.args.get('next')
             if not next_page or url_parse(next_page).netloc != '':
-                next_page = url_for('index')
+                next_page = url_for('home')
             
             return redirect(next_page)
         
